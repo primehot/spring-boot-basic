e734e77c5d7f1fccd246c7ac7ef20e5dfae70606
OlehT <oleh.tsyupa@gmail.com>
2017-12-01 13:49:29 +0100

    SpringBoot project init

---
+++ src/main/java/com/springdev/springdev/SpringdevApplication.java
@@ -0,0 +1,12 @@
+package com.springdev.springdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdevApplication.class, args);
+	}
+}
